476d9eff3ebc767e0c88b0f770ba57cfb9ed3e7c
Kersan <kailh.tak@gmail.com>
2023-01-20 18:57:14 +0100

    troche komend

--- spicier/cogs/admin.py
+++ spicier/cogs/admin.py
@@ -7,23 +7,43 @@ class AdminCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.group(name="admin", usage="[command]")
+    @commands.group(name="admin", usage="[command]", aliases=["a"])
     @commands.is_owner()
     async def admin(self, ctx):
         pass
 
-    @admin.command(name="reload")
+    @admin.command(name="reload", aliases=["r"])
     @commands.is_owner()
-    async def reload(self, ctx, cog: str):
+    async def reload_command(self, ctx, cog: str = None):
+        """Reloads a cog or all cogs."""
+
+        async def reloads(cogs: list):
+            for cog in cogs:
+                await self.bot.reload_extension(f"spicier.cogs.{cog}")
+            return (
+                f"Reloaded {len(cogs)} cogs!"
+                if len(cogs) > 1
+                else f"Reloaded {cogs[0]}"
+            )
+
+        msg = None
         try:
-            self.bot.reload_extension(f"spicier.cogs.{cog}")
-            await ctx.send(f"Reloaded {cog}")
+            msg = await reloads(
+                cog.split(",") if cog else [cog[:-3].lower() for cog in self.bot.cogs]
+            )
         except Exception as e:
             logging.error(e)
+
+        if not msg:
+            return await ctx.message.add_reaction("❌")
+
+        await ctx.message.add_reaction("✅")
+        return await ctx.reply(msg)
 
     @admin.command(name="sync")
     @commands.is_owner()
     async def sync(self, ctx):
+        """Syncs the command tree."""
         try:
             await self.bot.tree.sync()
             await ctx.message.add_reaction("✅")
--- spicier/cogs/music.py
+++ spicier/cogs/music.py
@@ -2,7 +2,20 @@ import logging
 from typing import Union
 
 import wavelink
+from discord import VoiceChannel
 from discord.ext import commands
+
+
+async def voice_check(ctx: commands.Context):
+    if not ctx.author.voice:
+        await ctx.reply("You not in vc!")
+        return False
+    if not ctx.voice_client:
+        return True
+    if ctx.author.voice.channel != ctx.voice_client.channel:
+        await ctx.reply("You not in my vc!")
+        return False
+    return True
 
 
 class MusicCog(commands.Cog):
@@ -19,42 +32,131 @@ class MusicCog(commands.Cog):
         """Event fired when a node has finished connecting."""
         logging.info(f"Node: <{node.identifier}> is ready!")
 
+    @commands.command(name="connect", aliases=["join"])
+    async def connect_command(
+        self, ctx: commands.Context, *, channel: VoiceChannel = None
+    ):
+        """Connect to a voice channel."""
+        try:
+            channel = channel or ctx.author.voice.channel
+        except AttributeError:
+            return await ctx.send(
+                "No voice channel to connect to. Please either provide one or join one."
+            )
+
+        vc: wavelink.Player = await channel.connect(cls=wavelink.Player)
+        return vc
+
     @commands.command()
-    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
+    @commands.check(voice_check)
+    async def play(
+        self, ctx: commands.Context, *, track: Union[wavelink.YouTubeTrack, str, None]
+    ):
         """Play a song with the given search query.
-
         If not connected, connect to our voice channel.
-        """
-        if ctx.author.voice is None:
-            return await ctx.send("Not in voice channel")
-        vc: wavelink.Player = (
-            ctx.voice_client
-            or await ctx.author.voice.channel.connect(cls=wavelink.Player)
-        )
-        if vc.queue.is_empty and not vc.is_playing():
-            await vc.play(search)
-        else:
-            await vc.queue.put_wait(search)
-            await ctx.send(f"Added `{search.title}` to the queue...", delete_after=10)
 
-    @commands.command()
-    async def skip(self, ctx: commands.Context):
-        """Skip the current song."""
+        When the query is a URL, we will attempt to play the song.
+        If not, we will attempt to search for a song.
+        """
+
+        final = None
+        vc: wavelink.Player = ctx.voice_client
+
+        if not vc:
+            vc = await self.connect_command(ctx, channel=ctx.author.voice.channel)
+
+        if not track and vc.is_paused:
+            await vc.resume()
+            return await self.resume_command(ctx)
+
+        if isinstance(track, wavelink.YouTubeTrack):
+            final = track
+
+        elif isinstance(track, str):
+            final = await wavelink.YouTubeTrack.search(query=track, return_first=True)
+
+        if not final:
+            return await ctx.send("No match found!")
+
+        if vc.track:
+            vc.queue.put(final)
+            return await ctx.send(f"Added to queue: {final.title}")
+
+        vc.queue.put(final)
+        await vc.play(vc.queue.get())
+        return await ctx.send(f"Playing: {final.title}")
+
+    @commands.command(name="queue", aliases=["q"])
+    async def queue_command(self, ctx: commands.Context):
+        """Show the current queue."""
+
         if not ctx.voice_client:
             return await ctx.send("Not playing rn!")
 
-        if not ctx.author.voice:
-            return await ctx.send("You not in vc!")
+        vc: wavelink.Player = ctx.voice_client
 
-        if ctx.author.voice.channel != ctx.voice_client.channel:
-            return await ctx.send("You not in my vc!")
+        if not vc.queue:
+            return await ctx.send("No songs queued!")
+
+        queue = "\n".join(f"{i} - {t.title}" for i, t in enumerate(vc.queue, start=1))
+        await ctx.send(f"```{queue}```")
+
+    @commands.command(name="skip", aliases=["s"])
+    @commands.check(voice_check)
+    async def skip_command(self, ctx: commands.Context):
+        """Skip the current song."""
 
         vc: wavelink.Player = (
             ctx.voice_client
             or await ctx.author.voice.channel.connect(cls=wavelink.Player)
         )
 
-        await vc.stop()
+        if not vc.queue:
+            await vc.stop()
+            return await ctx.send("No songs queued, stopping!")
+
+        await vc.play(vc.queue.get())
+        return await ctx.send(f"Skipped, now playing: {vc.track}")
+
+    @commands.command(name="pause", aliases=["stop"])
+    @commands.check(voice_check)
+    async def pause_command(self, ctx: commands.Context):
+        """Pause the current song."""
+
+        vc: wavelink.Player = (
+            ctx.voice_client
+            or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        )
+
+        await vc.pause()
+
+    @commands.command(name="resume")
+    @commands.check(voice_check)
+    async def resume_command(self, ctx: commands.Context):
+        """Resume the current song."""
+
+        vc: wavelink.Player = (
+            ctx.voice_client
+            or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        )
+
+        await vc.resume()
+
+    @commands.command(name="now_playing", aliases=["np"])
+    async def now_playing_command(self, ctx: commands.Context):
+        """Show the current song."""
+
+        vc: wavelink.Player = (
+            ctx.voice_client
+            or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        )
+
+        if not vc.track:
+            return await ctx.send("Nothing playing!")
+
+        return await ctx.send(
+            f"Now playing: {vc.track}\n{vc.position}/{vc.track.duration}"
+        )
 
 
 async def setup(bot):
